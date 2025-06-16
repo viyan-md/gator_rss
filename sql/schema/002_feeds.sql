29d75f83bf55d9077bc7a4f89fb723ab65c8bcf3
viyan-md <viyan.work@pm.me>
2025-06-17 01:47:16 +0200

    add migration for feeds

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE feeds (
+id UUID PRIMARY KEY,
+created_at TIMESTAMP NOT NULL,
+updated_at TIMESTAMP NOT NULL,
+name TEXT UNIQUE NOT NULL
+url TEXT UNIQUE NOT NULL,
+user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+);
+
+-- +goose Down
+DROP TABLE users;
